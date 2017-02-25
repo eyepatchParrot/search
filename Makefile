209b4b01e906175dcff01a1e9a75edd02e9fc8a9
eyepatchParrot <peter.vansandt@gmail.com>
2017-02-25 10:22:09 -0600

    Insertion vs Binary

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-GC=g++ -ggdb -Wall -std=c++11
+GC=g++ -g2 -Wall -std=c++11 -fno-omit-frame-pointer -flto
 
 profile: search.cc
 	$(GC) -DNDEBUG -Og search.cc -o $@
--- search.cc
+++ search.cc
@@ -38,19 +38,80 @@ int binSearch(const ll k, const std::vector<ll>& a) {
   return idx;
 }
 
+int binSearchBlk(const ll k, const std::vector<ll>& a) {
+  const int BLK = 8;
+  assert(BLK % 2 == 0);
+
+  unsigned l = 0, r = (BLK - 1 + a.size()) / BLK;
+  int ix = -1;
+
+  while (r - l > 1) {
+    assert(l < r);
+    assert(l+r >= r); // overflow check
+    unsigned mBlk = (l+r) / 2;
+    unsigned m = mBlk * BLK;
+    unsigned m2 = std::min(m + BLK - 1, (unsigned)a.size());
+    if (k < a[m]) {
+      r = mBlk;
+    } else if (k > a[m2]) {
+      l = mBlk+1;
+    } else {
+      l = r = mBlk;
+    }
+  }
+
+  l *= BLK;
+  r = std::min((unsigned)a.size(), l + BLK);
+  while (r - l > 1) {
+    unsigned m = (l + r) / 2;
+    if (k < a[m]) {
+      r = m;
+    } else if (k > a[m]) {
+      l = m+1;
+    } else {
+      ix = m;
+      break;
+    }
+  }
+  return ix;
+}
+
+int intSearch(const ll k, const std::vector<ll>& a) {
+  // r is inclusive
+  unsigned l = 0, r = a.size() - 1;
+  ll vL = a[l], vR = a[r];
+  int ix = -1;
+  while (r - l >= 0) {
+    assert(l <= r);    // ordering check
+    double p = (double)(k-vL) / (double)(vR - vL);
+    unsigned m = l + p * (r-l);
+    if (k < a[m]) {
+      r = m - 1;
+      vR = a[r];
+    } else if (k > a[m]) {
+      l = m + 1;
+      vL = a[l];
+    } else {
+      ix = m;
+      break;
+    }
+  }
+  return ix;
+}
+
 struct PerfStats {
   std::string name;
   std::vector<double> t;
   std::vector<ll> v;
 };
 
-PerfStats perfStats(std::vector<ll>& time, const std::vector<ll>& array, const std::vector<ll>& order, const std::string& name) {
+PerfStats perfStats(std::vector<ll>& time, const std::vector<ll>& array, const std::vector<std::tuple<ll, ll> >& order, const std::string& name) {
   ll nNums = array.size();
   std::vector<std::tuple<ll, int> > time_ix;
   ll sum = 0;
   for (ll i = 0; i < nNums; i++) {
-    int j = binSearch(order[i], array);
-    time_ix.push_back(std::make_tuple(time.back(), j));
+    time_ix.push_back(std::tuple<ll, ll>(time.back(), std::get<1>(order[i])));
+    //time_ix.push_back(std::make_tuple(time.back(), std::get<1>(order[i])));
     sum += time.back();
     time.pop_back();
   }
@@ -76,7 +137,8 @@ int main() {
   const int FLUSH_SZ = 1 << 28;
   ll nNums;
   unsigned A;
-  std::vector<ll> array, search;
+  std::vector<ll> array;
+  std::vector<std::tuple<ll, ll> > search;
   char* flush1 = new char[FLUSH_SZ];
   char* flush2 = new char[FLUSH_SZ];
 
@@ -88,7 +150,8 @@ int main() {
     loaded = 1 == scanf("%llu", &x);
     assert(loaded);
     array.push_back(x);
-    search.push_back(x);
+    search.push_back(std::tuple<ll, ll>(x, i));
+    //search.push_back(std::make_tuple(x, i));
   }
   std::vector<ll> t_lb(nNums), t_bs(nNums);
 
@@ -101,20 +164,38 @@ int main() {
   for (ll i = 0; i < nNums; i++) {
     ll st = __rdtsc();
     // can I beat STL bin search?
-    std::lower_bound(array.begin(), array.end(), search[i]);
+    std::lower_bound(array.begin(), array.end(), std::get<0>(search[i]));
     t_lb.push_back(__rdtscp(&A) - st);
   }
 
   memcpy(flush1, flush2, FLUSH_SZ);
   for (ll i = 0; i < nNums; i++) {
     ll st = __rdtsc();
-    binSearch(search[i], array);
+    binSearch(std::get<0>(search[i]), array);
     t_bs.push_back(__rdtscp(&A) - st);
+  }
+
+  std::vector<ll> t_bk;
+  memcpy(flush1, flush2, FLUSH_SZ);
+  for (ll i = 0; i < nNums; i++) {
+    ll st = __rdtsc();
+    binSearchBlk(std::get<0>(search[i]), array);
+    t_bk.push_back(__rdtscp(&A) - st);
+  }
+
+  std::vector<ll> t_it;
+  memcpy(flush1, flush2, FLUSH_SZ);
+  for (ll i = 0; i < nNums; i++) {
+    ll st = __rdtsc();
+    intSearch(std::get<0>(search[i]), array);
+    t_it.push_back(__rdtscp(&A) - st);
   }
 
   std::vector<PerfStats> ps;
   ps.push_back(perfStats(t_lb, array, search, "lb"));
   ps.push_back(perfStats(t_bs, array, search, "bs"));
+  ps.push_back(perfStats(t_bk, array, search, "bk"));
+  ps.push_back(perfStats(t_it, array, search, "it"));
 
   std::vector<std::string> fields {
     "name", "avg", "t90", "t99", "t999", "tMax", "v90", "v99", "v999", "vMax" };
