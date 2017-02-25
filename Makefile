953b994b1bccb7824b47f362a7675e164e3df682
eyepatchParrot <peter.vansandt@gmail.com>
2017-02-25 14:23:28 -0600

    interpolation tweaked

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-GC=g++ -g2 -Wall -std=c++11 -fno-omit-frame-pointer -flto
+GC=g++ -g2 -Wall -std=c++11 -fno-omit-frame-pointer -flto -ffast-math
 
 profile: search.cc
 	$(GC) -DNDEBUG -Og search.cc -o $@
--- search.cc
+++ search.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <tuple>
 #include <numeric>
 #include <cstring>
@@ -16,37 +17,65 @@
 // TODO remember that integer type matters. Parameterize?
 typedef unsigned long long ll;
 
-int binSearch(const ll k, const std::vector<ll>& a) {
+struct Search {
+  int ix, steps;
+};
+
+Search binSearch(const ll k, const std::vector<ll>& a) {
   unsigned l = 0;
   unsigned r = a.size();
-  int idx = -1;
+  Search s = Search{-1, 0};
 
   while (r - l > 0) {
     assert(l < r);    // ordering check
     assert(l+r >= r); // overflow check
     unsigned m = (l+r) / 2;
-    if (a[m] == k) {
-      idx = m;
-      break;
-    } else if (a[m] < k) {
+    s.steps++;
+    if (a[m] < k) {
       l = m + 1;
-    } else {
+    } else if (a[m] > k) {
       r = m;
+    } else {
+      s.ix = m;
+      break;
     }
   }
 
-  return idx;
+  return s;
 }
 
-int intSearch(const ll k, const std::vector<ll>& a) {
-  // r is inclusive
-  unsigned l = 0, r = a.size() - 1;
-  ll vL = a[l], vR = a[r];
-  int ix = -1;
-  while (r - l >= 0) {
-    assert(l <= r);    // ordering check
-    double p = (double)(k-vL) / (double)(vR - vL);
-    unsigned m = l + p * (r-l);
+struct IntStruct {
+  unsigned l, r;
+  double vL, vR, slope;
+
+  IntStruct(const std::vector<ll>& a) {
+    l = 0;
+    r = a.size() - 1;
+    vL = a[l];
+    vR = a[r];
+    slope = 1.0 / (vR - vL);
+  }
+};
+
+Search intSearch(const ll k, const std::vector<ll>& a, IntStruct& s) {
+  // can we change this to fix pt arithm?
+  // first priority is reducing number of lookups
+  // two ideas about systematically underestimating and overestimating
+  // 1. each repeated time you under-estimate, over-estimate by more the next time.
+  //    if you go on the other end, then reset and repeat.
+  // 2. Take the error by which you were off, and multiply your next estimate by that.
+  // 3. Look at euclid's alg and stuff like for for using error info to guide search
+  ll l = s.l, r = s.r;
+  double vL = s.vL, vR = s.vR, vK = k;
+  Search ret = Search{-1, 0};
+  assert(r - l >= 0); // assume non-empty vector
+  while (r - l > 0) {
+    double p =  1.0 / (vR - vL) * (vK - vL);
+    unsigned m = std::min(r, l + (unsigned)(p * (r-l)));
+//    if (k == a[12185329]) {
+//      printf("%u\n", m);
+//    }
+    ret.steps++;
     if (k < a[m]) {
       r = m - 1;
       vR = a[r];
@@ -54,11 +83,15 @@ int intSearch(const ll k, const std::vector<ll>& a) {
       l = m + 1;
       vL = a[l];
     } else {
-      ix = m;
-      break;
+      ret.ix = m;
+      return ret;
     }
   }
-  return ix;
+  if (k == a[l]) {
+    ret.ix = l;
+  }
+
+  return ret;
 }
 
 struct PerfStats {
@@ -116,53 +149,80 @@ int main() {
   std::srand(10);
   std::random_shuffle(search.begin(), search.end());
 
-  {
-    std::vector<ll> array(input);
-    for (ll i = 0; i < nNums; i++) {
-      ll st = __rdtsc();
-      std::lower_bound(array.begin(), array.end(), std::get<0>(search[i]));
-      t_lb.push_back(__rdtscp(&A) - st);
-    }
-  }
+//  {
+//    std::vector<ll> array(input);
+//    ll st = __rdtsc();
+//    for (ll i = 0; i < nNums; i++) {
+//      std::lower_bound(array.begin(), array.end(), std::get<0>(search[i]));
+////      t_lb.push_back(__rdtscp(&A) - st);
+//    }
+//    printf("%llu\n", __rdtscp(&A) - st);
+//  }
+//
+//  {
+//    std::vector<ll> array(input);
+//    int steps = -1;
+//    ll st = __rdtsc();
+//    for (ll i = 0; i < nNums; i++) {
+//      steps = std::max(steps, binSearch(std::get<0>(search[i]), array).steps);
+////      t_bs.push_back(__rdtscp(&A) - st);
+//    }
+//    printf("%llu %d bs\n", __rdtscp(&A) - st, steps);
+//  }
 
   {
     std::vector<ll> array(input);
+    IntStruct s(array);
+    int maxSteps = -1;
+    int maxStepIx = -1;
+    ll st = __rdtsc();
     for (ll i = 0; i < nNums; i++) {
-      ll st = __rdtsc();
-      binSearch(std::get<0>(search[i]), array);
-      t_bs.push_back(__rdtscp(&A) - st);
+      int steps = intSearch(std::get<0>(search[i]), array, s).steps;
+      if (steps > maxSteps) {
+        maxSteps = steps;
+        maxStepIx = std::get<1>(search[i]);
+      }
+//      t_it.push_back();
     }
+    printf("%llu %d %d is\n", __rdtscp(&A) - st, maxSteps, maxStepIx);
   }
 
-  std::vector<ll> t_it;
-  {
-    std::vector<ll> array(input);
-    for (ll i = 0; i < nNums; i++) {
-      ll st = __rdtsc();
-      intSearch(std::get<0>(search[i]), array);
-      t_it.push_back(__rdtscp(&A) - st);
-    }
-  }
+//  {
+//    std::vector<ll> array(input);
+//    IntStruct s(array);
+//    int maxSteps = -1;
+//    int maxStepIx = -1;
+//    ll st = __rdtsc();
+//    for (ll i = 0; i < nNums; i++) {
+//      int steps = intSearch(std::get<0>(search[i]), array, s).steps;
+//      if (steps > maxSteps) {
+//        maxSteps = steps;
+//        maxStepIx = std::get<1>(search[i]);
+//      }
+////      t_it.push_back();
+//    }
+//    printf("%llu %d %d is\n", __rdtscp(&A) - st, maxSteps, maxStepIx);
+//  }
 
-  std::vector<PerfStats> ps;
-  ps.push_back(perfStats(t_lb, input, search, "lb"));
-  ps.push_back(perfStats(t_bs, input, search, "bs"));
-  ps.push_back(perfStats(t_it, input, search, "it"));
-
-  std::vector<std::string> fields {
-    "name", "avg", "t90", "t99", "t999", "tMax", "v90", "v99", "v999", "vMax" };
-  for (unsigned i = 0; i < fields.size(); i++) {
-    printf("%s%s", i == 0 ? "" : ",", fields[i].c_str());
-  }
-  printf("\n");
-  for (unsigned i = 0; i < ps.size(); i++) {
-    printf("%s", ps[i].name.c_str());
-    for (double t : ps[i].t) {
-      printf(",%e", t);
-    }
-    for (ll v : ps[i].v) {
-      printf(",%llu", v);
-    }
-    printf("\n");
-  }
+//  std::vector<PerfStats> ps;
+//  ps.push_back(perfStats(t_lb, input, search, "lb"));
+//  ps.push_back(perfStats(t_bs, input, search, "bs"));
+//  ps.push_back(perfStats(t_it, input, search, "it"));
+//
+//  std::vector<std::string> fields {
+//    "name", "avg", "t90", "t99", "t999", "tMax", "v90", "v99", "v999", "vMax" };
+//  for (unsigned i = 0; i < fields.size(); i++) {
+//    printf("%s%s", i == 0 ? "" : ",", fields[i].c_str());
+//  }
+//  printf("\n");
+//  for (unsigned i = 0; i < ps.size(); i++) {
+//    printf("%s", ps[i].name.c_str());
+//    for (double t : ps[i].t) {
+//      printf(",%e", t);
+//    }
+//    for (ll v : ps[i].v) {
+//      printf(",%llu", v);
+//    }
+//    printf("\n");
+//  }
 }
