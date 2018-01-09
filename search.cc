f16b167e77cc5434ece4eb2833b026e63412cd41
eyepatchParrot <peter.vansandt@gmail.com>
2018-01-08 19:01:04 -0600

    Refactor input gathering

--- search.cc
+++ search.cc
@@ -4,6 +4,7 @@
 #include "bin.h"
 #include "lin.h"
 #include "util.h"
+#include "fib.h"
 
 #include <algorithm>
 #include <assert.h>
@@ -17,13 +18,49 @@
 #include <x86intrin.h>
 #include <unordered_map>
 
-std::vector<Key> randInts(int n, long seed) {
+std::vector<Key> randNums(int n, long seed) {
   std::mt19937_64 rng(seed);
   std::uniform_int_distribution<Key> dist(1, (1L<<63) - 2);
   std::vector<Key> v(n);
   for (auto& x : v) x = dist(rng);
   return v;
 }
+
+struct Input {
+  private:
+    struct KeyRandomSortedIndex {
+      Key key;
+      int random_index;
+      int sorted_index;
+
+      KeyRandomSortedIndex(Key k) { key = k; }
+    };
+  public:
+
+  std::vector<Key> keys;
+  std::vector<int> indexes;
+
+  Input(std::vector<Key> nums, int n_gets) : keys(nums.size()), indexes(n_gets) {
+#ifdef DUMP_INPUT
+    return;
+#endif
+
+    std::vector<KeyRandomSortedIndex> key_indexes(nums.begin(), nums.end());
+    for (int i = 0; i < key_indexes.size(); i++)
+      key_indexes[i].random_index = i;
+    std::sort(key_indexes.begin(), key_indexes.end(), [](auto l, auto r) {
+        return l.key < r.key; });
+    for (int i = 0; i < key_indexes.size(); i++)
+      key_indexes[i].sorted_index = i;
+
+    // save the sorted input while it's here
+    for (auto i = 0; i < nums.size(); i++) keys[i] = key_indexes[i].key;
+
+    std::sort(key_indexes.begin(), key_indexes.end(), [](auto l, auto r) {
+        return l.random_index < r.random_index; });
+    for (int i = 0; i < n_gets; i++) indexes[i] = key_indexes[i].sorted_index;
+  }
+};
 
 // ./x datasetSz seed nThds [benchmarks ...]
 int main(int argc, char *argv[]) {
@@ -34,7 +71,7 @@ int main(int argc, char *argv[]) {
   int argi = 1;
   int datasetSz = atoi(argv[argi++]);
   long seed = atol(argv[argi++]);
-  int nThreads = atoi(argv[argi++]);
+  int n_threads = atoi(argv[argi++]);
   std::vector<std::string> benchmarks;
   while (argi < argc) benchmarks.push_back(argv[argi++]);
 
@@ -42,37 +79,12 @@ int main(int argc, char *argv[]) {
 
   int nGets = SUBSET_SIZE < 1 ? datasetSz : SUBSET_SIZE;
 
-  // permute the items
-  //std::vector<Key> input;
-  auto input = randInts(datasetSz, seed);
+  Input input(randNums(datasetSz, seed), nGets);
+
 #ifdef DUMP_INPUT
   for (auto x : input) std::cout << x << '\n';
   return 0;
 #endif
-
-  std::vector<int> testIndexes(nGets);
-  {
-    //auto rndInput = randInts(datasetSz, seed);
-    // It should be faster to do three sorts than a sort and a search all
-    // even with the copies
-    std::vector<std::tuple<Key, int, int>> input_ix(input.size());
-    for (int i = 0; i < input.size(); i++)
-      input_ix[i] = std::make_tuple(input[i], i, 0);
-    std::sort(input_ix.begin(), input_ix.end());
-    assert(std::is_sorted(input_ix.begin(), input_ix.end()));
-
-    for (int i = 0; i < input.size(); i++)
-      std::get<2>(input_ix[i]) = i;
-    for (int i = 0; i < input.size(); i++) input[i] = std::get<0>(input_ix[i]);
-
-    auto cmp = [](auto _i_, auto _j_) { return std::get<1>(_i_) < std::get<1>(_j_); };
-    std::sort(input_ix.begin(), input_ix.end(), cmp);
-    assert(std::is_sorted(input_ix.begin(), input_ix.end(), cmp));
-
-    for (int i = 0; i < nGets; i++) testIndexes[i] = std::get<2>(input_ix[i]);
-  }
-  assert(std::is_sorted(input.begin(), input.end()));
-
   std::unordered_map<std::string, Benchmark*> benchmarkFns{
     {"b0", benchmark<B0>},
       {"b1", benchmark<B1>},
@@ -100,13 +112,14 @@ int main(int argc, char *argv[]) {
       {"b-sz-pow", benchmark<BinarySzPow>},
       {"b-sz-lin", benchmark<BinarySzLin>},
       {"b2", benchmark<B2>},
+      {"fib", benchmark<Fib>},
 
       {"oracle", benchmark<Oracle> }
   };
 
   std::vector<TestStats> tests;
   for (auto& benchmark : benchmarks) {
-    TestStats ts = benchmarkFns[benchmark](input, testIndexes, nThreads); 
+    TestStats ts = benchmarkFns[benchmark](input.keys, input.indexes, n_threads); 
     if (!ts.ok)
       std::cerr << "mess up " << seed << ' ' << benchmark << '\n';
     ts.name = benchmark;
@@ -118,12 +131,12 @@ int main(int argc, char *argv[]) {
     std::cout << (0 != i ? "," : "") << benchmarks[i];
   std::cout << '\n';
 
-  constexpr int nSamples = N_SAMPLES < 0 ? N_RUNS : std::min(N_RUNS, N_SAMPLES);
+  constexpr int n_samples = N_SAMPLES < 0 ? N_RUNS : std::min(N_RUNS, N_SAMPLES);
 #ifndef NSORT
   for (auto& t : tests)
-    std::partial_sort(t.ns.begin(), t.ns.begin() + nSamples, t.ns.end());
+    std::partial_sort(t.ns.begin(), t.ns.begin() + n_samples, t.ns.end());
 #endif
-  for (int runIx = 0; runIx < nSamples; runIx++, std::cout << '\n')
+  for (int runIx = 0; runIx < n_samples; runIx++, std::cout << '\n')
     for (int testIx = 0; testIx < tests.size(); testIx++)
       printf("%s%.3f", 0 != testIx ? "," : "", tests[testIx].ns[runIx]);
 }
